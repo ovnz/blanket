5cd1b2fe01a36bd3391b889d81cb0b01894b7a5b
marie <marie.retroposon@protonmail.com>
2020-12-28 10:08:35 +0100

    allow other SkinObjects to be used withing SkinObjects

--- skin_object.lua
+++ skin_object.lua
@@ -4,6 +4,7 @@
 -- dst objects can be either:
 -- * beatoraja dst tables
 -- * functions of the form `func(skin, origin_x, origin_y)`
+-- * other skin objects
 
 local SkinObject = {
     origin_x = 0, origin_y = 0, dst = {},
@@ -12,28 +13,39 @@ local SkinObject = {
         print("#self.dst = " .. #self.dst)
         for _,item in pairs(self.dst) do
             if type(item) == "table" then
-                local t_item = {}
+                if item.is_skin_object then
+                    -- item is a SkinObject
+                    local old_x, old_y = item.origin_x, item.origin_y
+                    item.origin_x = item.origin_x + self.origin_x
+                    item.origin_y = item.origin_y + self.origin_y
+                    item:apply(skin)
+                    item.origin_x, item.origin_y = old_x, old_y
+                else
+                    -- item is a DST call
+                    local t_item = {}
 
-                for key,val in pairs(item) do
-                    if key ~= "dst" then
-                        t_item[key] = val
+                    for key,val in pairs(item) do
+                        if key ~= "dst" then
+                            t_item[key] = val
+                        end
                     end
-                end
 
-                t_item.dst = {}
+                    t_item.dst = {}
 
-                for _,anim in pairs(item.dst) do
-                    local t_anim = {}
-                    for key,val in pairs(anim) do
-                        t_anim[key] = val
+                    for _,anim in pairs(item.dst) do
+                        local t_anim = {}
+                        for key,val in pairs(anim) do
+                            t_anim[key] = val
+                        end
+                        t_anim.x = anim.x + self.origin_x
+                        t_anim.y = anim.y + self.origin_y
+                        table.insert(t_item.dst, t_anim)
                     end
-                    t_anim.x = anim.x + self.origin_x
-                    t_anim.y = anim.y + self.origin_y
-                    table.insert(t_item.dst, t_anim)
-                end
 
-                table.insert(skin.destination, t_item)
+                    table.insert(skin.destination, t_item)
+                end
             elseif type(item) == "function" then
+                    -- item is a function
                 item(skin, self.origin_x, self.origin_y)
             end
         end
@@ -41,7 +53,7 @@ local SkinObject = {
 }
 
 function SkinObject:new(dst, x, y)
-    local obj = { origin_x = x, origin_y = y, dst = dst }
+    local obj = { origin_x = x, origin_y = y, dst = dst, is_skin_object = true }
     local meta = { __index = self }
     return setmetatable(obj, meta)
 end
